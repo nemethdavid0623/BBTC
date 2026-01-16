33414cd69270179d6f9de64b90ba717c782a8a3f
Davidomka23 <124671777+Davidomka@users.noreply.github.com>
2026-01-16 19:45:13 +0100

    Projekt, modellek, controllerek és az api létrehozása

---
+++ BBTC_Backend/app/Http/Controllers/UgyfelController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ugyfel;
+use Illuminate\Http\Request;
+
+class UgyfelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ugyfel $ugyfel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Ugyfel $ugyfel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Ugyfel $ugyfel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ugyfel $ugyfel)
+    {
+        //
+    }
+}
